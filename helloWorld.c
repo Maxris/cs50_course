9a16e766a62697ef18a49fa9ab866d8c8555d730
maxris <max.chris.13@outlook.fr>
2024-04-25 15:44:57 +0200

    square area calculator

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,6 @@
 #include <stdio.h> //libraries is just come code alrady written by someone before, this one include forexample the printf function
 
-int main(void) //main function,void because it oesn't return any value
+int main(void) //main function,void because it doesn't return any value
 {
     printf("hello, world!\n"); //instructions are between curly braces; \n moves down the cursor 
     return 0; //if we see any value != 0, it means there is a mistake in the code
