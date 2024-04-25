1614b087a6d0c77340ea5f934a9a94eef6c6d249
maxris <max.chris.13@outlook.fr>
2024-04-25 15:25:42 +0200

    first C program hello world

---
+++ helloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //libraries is just come code alrady written by someone before, this one include forexample the printf function
+
+int main(void) //main function,void because it oesn't return any value
+{
+    printf("hello, world!\n"); //instructions are between curly braces; \n moves down the cursor 
+    return 0; //if we see any value != 0, it means there is a mistake in the code
+}
+
+
+//manual.cs50.io for manual
